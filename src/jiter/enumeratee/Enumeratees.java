798cc080dedd5b35106ca11e14d0e0952ce7aec8
Alexander Lehmann <afwlehmann@googlemail.com>
2014-08-17 09:09:38 +0200

    First version of Enumeratee.map.

--- src/jiter/enumeratee/Enumeratees.java
+++ src/jiter/enumeratee/Enumeratees.java
@@ -5,5 +5,50 @@
 
 package jiter.enumeratee;
 
+import jiter.input.Input;
+import jiter.iteratee.Iteratee;
+
+import java.util.function.Function;
+
+import static jiter.iteratee.Iteratees.cont;
+import static jiter.iteratee.Iteratees.done;
+import static jiter.iteratee.Iteratees.error;
+
 public class Enumeratees {
+
+    public static abstract class CheckDone<From, To> extends Enumeratee<From, To> {
+        @Override
+        public <TT> Iteratee<From, Iteratee<To, TT>> apply(Iteratee<To, TT> iter) {
+            return iter.match(
+                _done -> done(_done, Input.empty()),
+                _cont -> kontinue(x -> _cont.apply(x)),
+                _err  -> done(_err, Input.empty())
+            );
+        }
+
+        abstract <TT> Iteratee<From, Iteratee<To, TT>> kontinue(Function<Input<To>, Iteratee<To, TT>> k);
+    }
+
+    public static <F, T> Enumeratee<F, T> map(Function<F, T> f) {
+        return new CheckDone<F, T>() {
+            <TT> Function<Input<F>, Iteratee<F, Iteratee<T, TT>>>
+            step(Function<Input<T>, Iteratee<T, TT>> k) {
+                return input -> input.match(
+                    _elt -> k.apply(Input.elem(f.apply(_elt))).match(
+                        _done -> done(_done, Input.empty()),
+                        _cont -> kontinue(k),
+                        _err -> done(_err, Input.empty())
+                    ),
+                    ()   -> cont(step(k)),
+                    ()   -> null
+                );
+            }
+
+            @Override
+            <TT> Iteratee<F, Iteratee<T, TT>> kontinue(Function<Input<T>, Iteratee<T, TT>> k) {
+                return cont(step(k));
+            }
+        };
+    }
+
 }
